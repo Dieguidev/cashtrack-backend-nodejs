a5144ab62e6c6d65fdb0f0d1627b03fc95bf910d
Dieguidev <diegogaraycullas@gmail.com>
2025-01-21 17:32:43 -0500

    agregar JWT_SECRET a la configuración de variables de entorno

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,13 +3,13 @@ import * as joi from 'joi'
 
 interface EnvVars {
   PORT: number
-  // JWT_SECRET: string
+  JWT_SECRET: string
   // FRONTEND_URL: string
 }
 
 const envsSchema = joi.object({
   PORT: joi.number().required(),
-  // JWT_SECRET: joi.string().required(),
+  JWT_SECRET: joi.string().required(),
   // FRONTEND_URL: joi.string().required(),
 })
 .unknown(true);
@@ -25,7 +25,7 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
-  // jwtSecret: envVars.JWT_SECRET,
+  jwtSecret: envVars.JWT_SECRET,
   // frontendUrl: envVars.FRONTEND_URL,
 }
 
