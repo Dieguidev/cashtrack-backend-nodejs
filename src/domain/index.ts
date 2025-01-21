063ad0144ab6060a6e2f75effd011ada3a702b57
Dieguidev <diegogaraycullas@gmail.com>
2025-01-21 18:19:10 -0500

    refactor: eliminar RegisterUserDto y agregar BudgetEntity con lógica de validación

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,1 +1,8 @@
 export * from './errors/custom.error';
+
+
+export * from './entities/budget.entity';
+
+
+
+export  * from './dto/create-budget.dto';
--- src/presentation/budgets.service.ts
+++ src/presentation/budgets.service.ts
@@ -1,9 +1,13 @@
 import { prisma } from "../data/prisma/prisma-db"
+import { BudgetEntity } from "../domain";
 
 export class Budgetservice {
   async getAllBudgets(){
     const budgets = await prisma.budget.findMany()
-    return budgets;
+    const budgetsEntity = budgets.map(budget=>(
+      BudgetEntity.fromJson(budget)
+    ))
+    return budgetsEntity;
   }
 
 
