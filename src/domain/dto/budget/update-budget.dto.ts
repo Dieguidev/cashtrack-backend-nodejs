4549b826761d643b15629d45446fb7d8715c994d
Dieguidev <diegogaraycullas@gmail.com>
2025-01-21 19:09:12 -0500

    refactor: actualizar GetBudgetByIdDto para incluir validación de cantidad y nombre

--- src/domain/dto/budget/update-budget.dto.ts
+++ src/domain/dto/budget/update-budget.dto.ts
@@ -3,14 +3,14 @@ import { UUIDAdapter } from '../../../config';
 export class GetBudgetByIdDto {
   private constructor(
     public id: string,
-    public name? : string,
-    public amount? : number,
+    public name?: string,
+    public amount?: number
   ) {}
 
   static create(object: {
     [key: string]: any;
   }): [{ [key: string]: string }?, GetBudgetByIdDto?] {
-    const { id,name, amount } = object;
+    const { id, name, amount } = object;
 
     const errors: { [key: string]: string } = {};
 
@@ -18,16 +18,18 @@ export class GetBudgetByIdDto {
     if (id && !UUIDAdapter.validate(id)) {
       errors.id = 'Invalid ID';
     }
-    if(name && typeof name !== 'string') errors.name = 'Name must be a string';
-    if(!name && ! amount){
+    if (name && typeof name !== 'string') errors.name = 'Name must be a string';
+    if (!name && !amount) {
       errors.data = 'Missing data';
     }
-
+    if (amount && typeof amount !== 'number')
+      errors.amount = 'Amount must be a number';
+    if (amount && amount <= 0) errors.amount = 'Amount must be greater than 0';
 
     if (Object.keys(errors).length > 0) {
       return [errors];
     }
 
-    return [undefined, new GetBudgetByIdDto(id)];
+    return [undefined, new GetBudgetByIdDto(id, name, amount)];
   }
 }
