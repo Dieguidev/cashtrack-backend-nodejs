350573286cebf422e1593d18562092dc904891fe
Dieguidev <diegogaraycullas@gmail.com>
2025-01-25 12:25:28 -0500

    feat: agregar controlador y rutas para la gestión de usuarios con validación de JWT

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -2,15 +2,16 @@ import { Router } from "express";
 import { BudgetRoutes } from "./budget/budget.routes";
 import { ExpenseRoutes } from "./expense/expense.routes";
 import { AuthRoutes } from "./auth/auth.routes";
+import { UserRoutes } from "./user/user.routes";
 
 export class AppRoutes {
   static get routes():Router {
     const router = Router();
 
     router.use('/auth', AuthRoutes.routes)
-    // router.use('/api/user', UserRoutes.routes)
     router.use('/budget', BudgetRoutes.router)
     router.use('/expense', ExpenseRoutes.router)
+    router.use('/user', UserRoutes.routes)
 
     router.get('/', (req, res) => {
       res.json({
---
+++ src/presentation/user/user.service.ts
@@ -0,0 +1,9 @@
+import { User } from "@prisma/client";
+import { UserEntity } from "../../domain/entities/user.entity";
+
+
+export class UserService {
+  async getUserByJWT(user: User) {
+    return UserEntity.fromJson(user);
+  }
+}
