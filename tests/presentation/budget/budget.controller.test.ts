de814674430ada4c57e6322b05b17f4d3ee9431c
Dieguidev <diegogaraycullas@gmail.com>
2025-01-25 18:08:14 -0500

    Generando un Mock

--- tests/presentation/budget/budget.controller.test.ts
+++ tests/presentation/budget/budget.controller.test.ts
@@ -1,8 +1,20 @@
 import { budgets } from "./mocks/budgets"
+import {createRequest, createResponse} from "node-mocks-http"
+import { BudgetController } from '../../../src/presentation/budget/budget.controller';
+import { Budgetservice } from "../../../src/presentation/budget/budgets.service";
+
+const budgetService = new Budgetservice()
+const budgetController = new BudgetController(budgetService)
 
 describe("BudgetController", () => {
-  it("should retrieve 3 budgets", () => {
-    expect(budgets).toHaveLength(3)
-    expect(budgets).not.toHaveLength(0)
+  it("should retrieve 3 budgets", async () => {
+    const req = createRequest({
+      method: "GET",
+      url: "/api//budget",
+      user: {id: "826f3d07-8dd8-4269-8144-ce4895928b19"}
+    })
+    const res = createResponse()
+    await budgetController.getAllBudgets(req, res)
+
   })
 })
