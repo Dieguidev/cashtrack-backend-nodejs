c720d1ca6b3d5988a91d66a49063d85af5e8ad74
Dieguidev <diegogaraycullas@gmail.com>
2025-01-24 22:52:55 -0500

    feat: agregar DTO para actualizar gastos con validaciones de nombre y monto

---
+++ src/domain/dto/expense/update-expense.dto.ts
@@ -0,0 +1,27 @@
+import { UUIDAdapter } from '../../../config';
+
+export class UpdateExpenseDto {
+  private constructor(public name?: string, public amount?: number) {}
+
+  static create(object: {
+    [key: string]: any;
+  }): [{ [key: string]: string }?, UpdateExpenseDto?] {
+    const { name, amount } = object;
+
+    const errors: { [key: string]: string } = {};
+
+    if (name && typeof name !== 'string') errors.name = 'Name must be a string';
+    if (!name && !amount) {
+      errors.data = 'Missing data';
+    }
+    if (amount && typeof amount !== 'number')
+      errors.amount = 'Amount must be a number';
+    if (amount <= 0) errors.amount = 'Amount must be greater than 0';
+
+    if (Object.keys(errors).length > 0) {
+      return [errors];
+    }
+
+    return [undefined, new UpdateExpenseDto(name, amount)];
+  }
+}
