ddbd1d588226123cb6306fca1ba8db503ab57a9f
Dieguidev <diegogaraycullas@gmail.com>
2025-01-25 12:48:24 -0500

    feat: agregar funcionalidad para actualizar la contraseña del usuario actual con validación

--- src/presentation/user/user.routes.ts
+++ src/presentation/user/user.routes.ts
@@ -19,6 +19,8 @@ export class UserRoutes {
       controller.getUserByJWT
     );
 
+    router.post('/update-password', [AuthMiddleware.validateJWT], controller.updateCurrentUserPassword)
+
     return router;
   }
 }
