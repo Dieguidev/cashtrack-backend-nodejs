c3ec9e5bb10913e86792d08aaf1077b3e9328b73
Dieguidev <diegogaraycullas@gmail.com>
2025-01-24 23:29:21 -0500

    feat: reestructurar migraciones y agregar modelo de usuario con relaciones

--- prisma/migrations/20250125041509_buget_cascade_delete_expenses/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `expense` DROP FOREIGN KEY `Expense_budgetId_fkey`;
-
--- DropIndex
-DROP INDEX `Expense_budgetId_fkey` ON `expense`;
-
--- AddForeignKey
-ALTER TABLE `Expense` ADD CONSTRAINT `Expense_budgetId_fkey` FOREIGN KEY (`budgetId`) REFERENCES `Budget`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
